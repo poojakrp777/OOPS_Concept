c4586237d51519d9b5c40670cece77765d6d80b2
poojakrp777 <poojakrp777@gmail.com>
2023-04-23 12:19:43 +0530

    [POOJA] OOPs Problems

---
+++ Example.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Concept
+{
+    abstract class Employee //abstract class
+    {
+        public abstract int GetSalary(); //abstract method
+    }
+
+    class Developer : Employee
+    {
+        private int salary;
+        public Developer(int s)
+        {
+            salary = s;
+        }
+        public override int GetSalary()
+        {
+            return salary;
+        }
+    }
+
+    class Driver : Employee
+    {
+        private int salary;
+        public Driver(int s)
+        {
+            salary = s;
+        }
+        public override int GetSalary()
+        {
+            return salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Concept
+{
+    
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Developer d1 = new Developer(5000);
+            Driver d2 = new Driver(3000);
+
+            int a, b;
+
+            a = d1.GetSalary();
+            b = d2.GetSalary();
+            Console.WriteLine($"Salary of developer : {a}");
+            Console.WriteLine($"Salary of driver : {b}");
+            Console.ReadLine();
+        }
+    }
+}
